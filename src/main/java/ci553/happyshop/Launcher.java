556d45f08b15cdc84a1a9d51cef1861787eb6499
Ammar-2007-ux <ammarhishama@gmail.com>
2025-12-27 10:53:52 +0000

    Separate customer and warehouse views using dedicated launchers

--- src/main/java/ci553/happyshop/Launcher.java
+++ src/main/java/ci553/happyshop/Launcher.java
@@ -1,11 +1,18 @@
 package ci553.happyshop;
 
-import ci553.happyshop.client.customer.CustomerClient;
+import ci553.happyshop.client.warehouse.WarehouseClient;
 import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Launcher {
+public class Launcher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Launch ONLY the Warehouse window
+        new WarehouseClient().start(primaryStage);
+    }
 
     public static void main(String[] args) {
-        Application.launch(CustomerClient.class, args);
+        launch(args);
     }
 }
